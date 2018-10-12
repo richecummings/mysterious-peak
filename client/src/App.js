5fe4d18f985f4383d77ebcc0e1cc199276be16c1
Richard Cummings <richardecummings@gmail.com>
2018-10-11 22:11:59 -0400

    Setting up setupProxy

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Hi there</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="/auth/google"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Sign In With Google
           </a>
         </header>
       </div>

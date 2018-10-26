f69a484cb2ec087f69f491e3091bd75abbb704fa
Richard Cummings <richardecummings@gmail.com>
2018-10-25 22:25:09 -0400

    Fields from config

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,38 +1,29 @@
 // SurveyForm shows a form for a user to add input
-import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
-import SurveyField from './SurveyField';
+import _ from "lodash";
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import SurveyField from "./SurveyField";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "email" }
+];
 
 class SurveyForm extends Component {
   renderFields() {
-    return (
-      <div>
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
         <Field
-          label="Survey Title"
-          type="text"
-          name="title"
+          key={name}
           component={SurveyField}
-        />
-        <Field
-          label="Subject Line"
           type="text"
-          name="subject"
-          component={SurveyField}
+          label={label}
+          name={name}
         />
-        <Field
-          label="Email Body"
-          type="text"
-          name="body"
-          component={SurveyField}
-        />
-        <Field
-          label="Recipient List"
-          type="text"
-          name="emails"
-          component={SurveyField}
-        />
-      </div>
-    );
+      );
+    });
   }
 
   render() {
@@ -48,5 +39,5 @@ class SurveyForm extends Component {
 }
 
 export default reduxForm({
-  form: 'surveyForm'
+  form: "surveyForm"
 })(SurveyForm);

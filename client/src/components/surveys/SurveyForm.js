0f6c97657fa4e541d10a37553240091d91f8e935
Richard Cummings <richardecummings@gmail.com>
2018-10-28 19:55:44 -0400

    Displaying invalid emails

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,12 +4,13 @@ import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
 
 const FIELDS = [
   { label: "Survey Title", name: "title" },
   { label: "Subject Line", name: "subject" },
   { label: "Email Body", name: "body" },
-  { label: "Recipient List", name: "email" }
+  { label: "Recipient List", name: "emails" }
 ];
 
 class SurveyForm extends Component {
@@ -47,6 +48,8 @@ class SurveyForm extends Component {
 
 function validate(values) {
   const errors = {};
+
+  errors.emails = validateEmails(values.emails || "");
 
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {

a2d516c97f9198cbcc00f3466e2bc9c51d4d244a
Richard Cummings <richardecummings@gmail.com>
2018-11-04 13:36:48 -0500

    Testing the query

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -38,7 +38,7 @@ module.exports = app => {
             $inc: { [choice]: 1 },
             $set: { "recipients.$.responded": true }
           }
-        );
+        ).exec();
       })
       .value();
 
